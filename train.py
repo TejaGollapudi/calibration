db7cdd5c44f0fa726444a10b9666f5765abd9b05
Sivateja Gollapudi <tejagtz10@gmail.com>
2021-04-06 13:19:41 -0500

    'fixed typo'

--- train.py
+++ train.py
@@ -455,7 +455,7 @@ if args.test_path:
     print(f'test samples = {len(test_dataset)}')
 
 if args.out_test_path:
-    out_test_dataset=TextDataset(args.out_test_dataset,MNLIProcessor)
+    out_test_dataset=TextDataset(args.out_test_path,MNLIProcessor)
     print(f'out domain test samples = {len(out_test_dataset)}')
 
 if args.do_train:
